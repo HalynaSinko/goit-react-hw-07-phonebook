56aa4f3fdccea699c9ecc2df1c7808bace4e5486
Halyna Sinko <galinasinko14@gmail.com>
2021-09-12 17:13:04 +0300

    go back

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import {
   fetchContactsRequest,
   fetchContactsSuccess,
@@ -9,42 +11,39 @@ import {
   removeContactSuccess,
   removeContactError,
 } from "./contacts-actions";
-import * as contactsApi from "../../services/contacts-api";
 
-const fetchContacts = () => async (dispatch) => {
+const BASE_URL = "http://localhost:4040";
+
+axios.defaults.baseURL = BASE_URL;
+
+const fetchContacts = () => (dispatch) => {
   dispatch(fetchContactsRequest());
-
-  try {
-    const { data } = await contactsApi.fetchAllContacts();
-
-    dispatch(fetchContactsSuccess(data));
-  } catch (error) {
-    dispatch(fetchContactsError(error));
-  }
+  axios
+    .get("/contacts")
+    .then(({ data }) => {
+      dispatch(fetchContactsSuccess(data));
+    })
+    .catch((error) => dispatch(fetchContactsError(error)));
 };
 
 const addContact =
   ({ name, number }) =>
-  async (dispatch) => {
-    dispatch(addContactRequest());
+  (dispatch) => {
+    const contact = { name, number };
 
-    try {
-      const { data } = await contactsApi.addNewContact({ name, number });
-      dispatch(addContactSuccess(data));
-    } catch (error) {
-      dispatch(addContactError(error));
-    }
+    dispatch(addContactRequest());
+    axios
+      .post("/contacts", contact)
+      .then(({ data }) => dispatch(addContactSuccess(data)))
+      .catch((error) => dispatch(addContactError(error)));
   };
 
-const removeContact = (contactId) => async (dispatch) => {
+const removeContact = (contactId) => (dispatch) => {
   dispatch(removeContactRequest());
-
-  try {
-    await contactsApi.removeContactById(contactId);
-    dispatch(removeContactSuccess(contactId));
-  } catch (error) {
-    dispatch(removeContactError(error));
-  }
+  axios
+    .delete(`/contacts/${contactId}`)
+    .then(() => dispatch(removeContactSuccess(contactId)))
+    .catch((error) => dispatch(removeContactError(error)));
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -6,7 +6,7 @@ export const getContacts = (state) => state.contacts.items;
 
 export const getLoading = (state) => state.contacts.loading;
 
-export const getError = (state) => state.contacts.error;
+// export const getError = (state) => state.contacts.error;
 
 export const getVisibleContacts = createSelector(
   [getContacts, getFilter],
